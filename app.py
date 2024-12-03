819c1b10ec29f3b72206c63509695b122dfa9fe0
meandyo <a0910134517@gmail.com>
2024-12-03 20:09:45 +0800

    新增網頁按鈕功能

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask!"
+    return render_template('home.html')
 
 @app.route('/next')
 def next_page():
-    return "<h1> Welcome to the next page! <h1>"
+    return "<h1>Welcome to the next page!</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
